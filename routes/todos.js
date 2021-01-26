b10d94854cfa70d5ae88277c651b1b6724208ae3
brianwlego <legobw@gmail.com>
2021-01-26 16:36:33 -0500

    finished rest routes

---
+++ routes/todos.js
@@ -0,0 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+let todos = [];
+const router = express_1.Router();
+router.get('/', (req, res, next) => {
+    res.status(200).json({ todos: todos });
+});
+router.post('/todo', (req, res, next) => {
+    const newTodo = {
+        id: new Date().toISOString(),
+        text: req.body.text
+    };
+    todos.push(newTodo);
+    res.status(201).json({ message: 'Added Todo', todos: todos });
+});
+router.put('/todo/:id', (req, res, next) => {
+    const tid = req.params.id;
+    const todoIdx = todos.findIndex(item => item.id === tid);
+    if (todoIdx >= 0) {
+        todos[todoIdx] = { id: todos[todoIdx].id, text: req.body.text };
+        res.status(200).json({ message: 'Updated Todo', todos: todos });
+    }
+    else {
+        res.status(404).json({ message: "Could not find todo for this id" });
+    }
+});
+router.delete('/todo/:id', (req, res, next) => {
+    todos = todos.filter(t => t.id !== req.params.id);
+    res.status(200).json({ message: 'Todo deleted' });
+});
+exports.default = router;
--- routes/todos.ts
+++ routes/todos.ts
@@ -1,31 +1,46 @@
 import { Router } from 'express'
+import { type } from 'os';
 import { Todo } from '../model/todo'
 
 
 
-const todos:Todo[]  = [];
+let todos:Todo[]  = [];
+
+type RequestBody = {text: string}
+type RequestParams = {id: string}
 
 const router = Router();
 
 router.get('/', (req, res, next) => {
   res.status(200).json({ todos: todos})
 })
+
 router.post('/todo', (req, res, next) => {
+  const body = req.body as RequestBody
   const newTodo:Todo = {
     id: new Date().toISOString(), 
-    text: req.body.text
+    text: body.text
   }
   todos.push(newTodo)
+
+  res.status(201).json({message: 'Added Todo', todos: todos})
 })
+
 router.put('/todo/:id', (req, res, next) => {
-  const tid = req.params.id
+  const params = req.params as RequestParams
+  const tid = params.id
   const todoIdx = todos.findIndex(item => item.id === tid)
   if (todoIdx >= 0){
     todos[todoIdx] = { id:todos[todoIdx].id, text: req.body.text }
+    res.status(200).json({message: 'Updated Todo', todos: todos})
   } else {
-    res.status(404).json({message: "Todo not valid"})
+    res.status(404).json({message: "Could not find todo for this id"})
   }
+})
 
+router.delete('/todo/:id', (req, res, next) => {
+  todos = todos.filter(t => t.id !== req.params.id)
+  res.status(200).json({message: 'Todo deleted'})
 })
 
 
