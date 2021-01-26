22c78c6208e833539d28858fa0ccb5343bd812d0
brianwlego <legobw@gmail.com>
2021-01-25 22:40:21 -0500

    working with assigning types

---
+++ app.js
@@ -0,0 +1,5 @@
+var num1Element = document.getElementById('num1');
+function add(num1, num2) {
+    return num1 + num2;
+}
+add(1, 6);
---
+++ app.ts
@@ -0,0 +1,12 @@
+const num1Element = document.getElementById('num1')
+const num2Element = document.getElementById('num2')
+const btn = document.querySelector('button')
+
+function add(num1:number, num2:number){
+  return num1 + num2;
+}
+
+btn.addEventListener('click', () => {
+  
+})
+
