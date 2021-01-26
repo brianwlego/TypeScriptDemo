326615457e804f1cb20f449eadd8fa6a94851935
brianwlego <legobw@gmail.com>
2021-01-25 23:40:56 -0500

    converting to rest api

--- app.js
+++ app.js
@@ -1,41 +1,5 @@
-"use strict";
-const num1Element = document.getElementById('num1');
-const num2Element = document.getElementById('num2');
-const btn = document.querySelector('button');
-const numResults = [];
-const textResults = [];
-function add(num1, num2) {
-    if (typeof num1 === 'number' && typeof num2 === 'number') {
-        return num1 + num2;
-    }
-    else if (typeof num1 === 'string' && typeof num2 === 'string') {
-        return num1 + num2;
-    }
-    else {
-        return +num1 + +num2;
-    }
-}
-function printResult(resultObj) {
-    console.log(resultObj.val, resultObj.timestamp);
-}
-btn.addEventListener('click', () => {
-    const num1 = num1Element.value;
-    const num2 = num2Element.value;
-    const result = add(+num1, +num2);
-    numResults.push(result);
-    const stringResult = add(num1, num2);
-    textResults.push(stringResult);
-    console.log(result);
-    console.log(stringResult);
-    printResult({ val: result, timestamp: new Date() });
-    console.log(numResults);
-    console.log(textResults);
-});
-const myPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('It worked!');
-    }, 1000);
-});
-myPromise.then(result => {
-    console.log(result.split('w'));
-});
+const express = require('express')
+
+const app = express();
+
+app.listen(3000)
